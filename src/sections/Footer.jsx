7a04a7519d05fdb1a10c4d1b558ffd13fbec64c3
Mohamed <mohamedkondombo135@gmail.com>
2025-06-14 20:53:19 -0400

    routes fix

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router";
 const Footer = () => {
   return (
     <footer className="bg-dark-green px-[24px] py-[66px] md:px-[40px] md:py-[56px] lg:px-[165px] lg:py-[48px]">
-      <div className="mx-auto grid max-w-[1200px] grid-cols-1 gap-[20px] md:grid-cols-2">
-        <nav className="flex flex-col items-center gap-[24px] bg-green-400 md:items-start md:gap-[53px]">
+      <div className="mx-auto grid max-w-[1200px] grid-cols-1 gap-[20px] md:grid-cols-2 xl:grid-cols-3 xl:items-start">
+        <nav className="flex flex-col items-center gap-[24px] md:items-start md:gap-[53px]">
           <img src="/src/assets/logo.svg" alt="" width={96} height={24} />
           <ul className="flex gap-[24px] text-[15px] leading-[25px] font-bold text-white">
             <li>
@@ -16,14 +16,14 @@ const Footer = () => {
           </ul>
         </nav>
 
-        <div className="flex flex-col items-center bg-yellow-200 text-[15px] leading-[25px] font-semibold text-[#99a8a9] md:items-end">
+        <div className="flex flex-col items-center text-[15px] leading-[25px] font-semibold text-[#99a8a9] md:items-end xl:items-start">
           <p>987 Hillcrest Lane</p>
           <p>Irvine, CA</p>
           <p>California 92714</p>
           <p>Call Us : 949-833-7432</p>
         </div>
 
-        <div className="mt-[16px] flex flex-col items-center gap-[16px] bg-amber-500 md:col-span-2 md:flex-row md:justify-between">
+        <div className="mt-[16px] flex flex-col items-center gap-[16px] md:col-span-2 md:flex-row md:justify-between xl:col-start-3 xl:mt-[0] xl:h-full xl:flex-col">
           <div className="flex gap-[16px]">
             <img src="/src/assets/icon-facebook.svg" alt="" />
             <img src="/src/assets/icon-pinterest.svg" alt="" />
